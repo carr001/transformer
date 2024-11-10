ecbe1e228af08450bdb4f4b8a9799475fc4ca1b3
yackub <yackub563@mail.ru>
2024-11-10 21:55:57 +0300

    fix error with mask (now mask shape is corresponds to Q * K_transposed shape)

--- model/dot_product_attention.py
+++ model/dot_product_attention.py
@@ -18,20 +18,23 @@ class DotProductAttention(nn.Module):
 
     def forward(self, x_query, x_key, x_value, mask=None):
         query = self._w_query(x_query)
-        key = self._w_key(x_key)
-        key.transpose_(-2, -1)  # Transpose last and pre-last dims (keys of tokens of sequence)
+        key_transposed = self._w_key(x_key)  # It will be transposed on the next step
+        key_transposed.transpose_(-2, -1)  # Transpose last and pre-last dims (keys of tokens of sequence)
         value = self._w_value(x_value)
 
         if mask is None:
             scores = nn.functional.softmax(
-                torch.matmul(query, key) / self._sqrt_d_head,
+                torch.matmul(query, key_transposed) / self._sqrt_d_head,
                 dim=-1
             )  # Softmax along rows - softmax along scores for every token of query (sum of scores for query token = 1)
         else:
             scores = nn.functional.softmax(
-                (torch.matmul(query, key) / self._sqrt_d_head) + mask,
+                (torch.matmul(query, key_transposed) / self._sqrt_d_head) + mask[:query.shape[-2], :key_transposed.shape[-1]],
                 dim=-1
             )  # Softmax along rows - softmax along scores for every token of query (sum of scores for query token = 1)
 
         attention = torch.matmul(scores, value)
         return attention
+
+    # query.shape[-2] is the amount of query tokens, key_transposed.shape[-1] is the amount of key tokens
+    # (in both batch and one-example inputs)
