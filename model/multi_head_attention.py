fc0c6bfad45e7db1abf0a9a9989032be38dfcc07
yackub <yackub563@mail.ru>
2025-02-22 12:57:43 +0300

    make parallel attention heads

--- model/multi_head_attention.py
+++ model/multi_head_attention.py
@@ -1,37 +1,103 @@
 # Author: Vodohleb04
 import torch
 from torch import nn
-from model.dot_product_attention import DotProductAttention
 
 
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, d_model, d_head, device, dtype):
-        if d_head > d_model:
-            raise ValueError(f"Expected d_head be less than d_model")
-        if d_model % d_head != 0:
-            raise ValueError(f"Expected d_model to be divisible by d_head without remainder")
-
+    def __init__(self, d_model, num_heads, device, dtype):
         super().__init__()
-        heads_amount = int(d_model / d_head)
+        if not d_model % num_heads == 0:
+            raise ValueError("Expected d_model is divided by num_heads without remainder")
 
-        self._attention_head_list = nn.ModuleList(
-            [DotProductAttention(d_model, d_head, device, dtype) for _ in range(heads_amount)]
-        )
+        self._d_head = d_model // num_heads
+        self._num_heads = num_heads
 
-        self._w0 = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)  # concat_heads * w0
+        self._w_query = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)
+        self._w_key = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)
+        self._w_value = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)
+        self._w0 = nn.Linear(d_model, d_model, bias=False, device=device, dtype=dtype)
 
 
-    def forward(self, x_query, x_key, x_value, mask=None):
-        head_result_list = [None for _ in range(len(self._attention_head_list))]
+    def dot_product_attention(self, query, key, value, dropout_p=0, mask=None):
+        scores = torch.matmul(query, key.transpose(-2, -1)) / self._sqrt_key_d
+        if mask is not None:
+            scores += mask
 
-        for i, attention_head in enumerate(self._attention_head_list):
-            head_result_list[i] = attention_head(x_query, x_key, x_value, mask=mask)
-        concat_heads = torch.concat(head_result_list, dim=-1)  # heads of corresponding examples from batch are concatenated
-        # (head11 + head12 + head13), (head21 + head22 + head23), ...
+        scores = nn.functional.softmax(scores, dim=-1)
+        scores = nn.functional.dropout(scores, p=dropout_p, training=self.training)
 
-        multi_head_attention = self._w0(concat_heads)
+        attention = torch.matmul(scores, value)
+        return attention
+
+
+    def _batch_forward(self, query_x, key_x, value_x, dropout_p, mask):
+        batch_size, seq_len = query_x.size(0), query_x.size(1)
+
+        query = self._w_query(query_x)
+        # divide to heads (every head is counted with individual parameters, but in common matrix)
+        query = query.view(batch_size, seq_len, self._num_heads, self._d_head)
+        # shape (batch_size, num_heads, seq_len, d_head) is needed to make attention across sequence for every head
+        query = query.transpose(-3, -2)
+
+        key = self._w_key(key_x)
+        key = key.view(batch_size, seq_len, self._num_heads, self._d_head)
+        key = key.transpose(-3, -2)
+
+        value = self._w_value(value_x)
+        value = value.view(batch_size, seq_len, self._num_heads, self._d_head)
+        value = value.transpose(-3, -2)
+
+        multi_head_attention = self.dot_product_attention(query, key, value, dropout_p, mask)
+        # return shape to (batch_size, seq_len, num_heads, d_head)
+        multi_head_attention = multi_head_attention.transpose(-3, -2).contiguous()
+        # return shape to (batch_size, seq_len, d_model)
+        multi_head_attention = multi_head_attention.view(batch_size, seq_len, self._num_heads * self._d_head)
+        multi_head_attention = self._w0(multi_head_attention)
         return multi_head_attention
+
+
+    def _no_batch_forward(self, query_x, key_x, value_x, dropout_p, mask):
+        seq_len = query_x.size(0)
+
+        query = self._w_query(query_x)
+        # divide to heads (every head is counted with individual parameters, but in common matrix)
+        query = query.view(seq_len, self._num_heads, self._d_head)
+        # shape (num_heads, seq_len, d_head) is needed to make attention across sequence for every head
+        query = query.transpose(-3, -2)
+
+        key = self._w_key(key_x)
+        key = key.view(seq_len, self._num_heads, self._d_head)
+        key = key.transpose(-3, -2)
+
+        value = self._w_value(value_x)
+        value = value.view(seq_len, self._num_heads, self._d_head)
+        value = value.transpose(-3, -2)
+
+        multi_head_attention = self.dot_product_attention(query, key, value, dropout_p, mask)
+        # return shape to (seq_len, num_heads, d_head)
+        multi_head_attention = multi_head_attention.transpose(-3, -2).contiguous()
+        # return shape to (seq_len, d_model)
+        multi_head_attention = multi_head_attention.view(seq_len, self._num_heads * self._d_head)
+        multi_head_attention = self._w0(multi_head_attention)
+        return multi_head_attention
+
+
+    def forward(self, query_x, key_x, value_x, dropout_p=0, mask=None):
+        if mask is not None:
+            # Same mask applied to all h heads.
+            mask = mask.unsqueeze(-3)
+
+        if len(query_x.shape) == 3:
+            return self._no_batch_forward(query_x, key_x, value_x, dropout_p, mask)
+        elif len(query_x.shape) == 4:
+            return self._batch_forward(query_x, key_x, value_x, dropout_p, mask)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -53,7 +119,7 @@ if __name__ == "__main__":
         device=device, dtype=dtype
     )
 
-    mha = MultiHeadAttention(6, 2, 3, device=device, dtype=dtype, masked=True)
+    mha = MultiHeadAttention(6, 2, device=device, dtype=dtype, masked=True)
     res = mha(x, x, x)
     res0 = mha(x[0], x[0], x[0])
     print(f"res[0]: {res[0]}")
